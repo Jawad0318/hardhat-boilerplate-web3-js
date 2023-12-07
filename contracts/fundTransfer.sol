fa6a3862e757080c43505003b4c70727e2dfaf9e
jawad <jawadaltaf142@gmail.com>
2023-12-07 18:39:42 +0500

    Writing smart contract

---
+++ contracts/fundTransfer.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.9;
+
+// create create a contract using key word contract
+contract tokenTransfer {
+    uint256 public withDrawlDate ;
+    address payable public owner;
+    event withDrawl (uint256 amount , uint256 when );
+    constructor(uint235 _withDrawlDate) public {
+        require(block.timestamp < _withDrawlDate, "With Drawl date must be in future");
+        withDrawlDate = _withDrawlDate;
+        owner = payable(msg.sender);
+    }
+
+function withDrawl() payable public{
+    require (block.timestamp < withDrawlDate, "You can not with draw it now");
+    require(msg.sender == owner,"Only owner can with draw");
+    emit withDrawl(address(this).balance , block.timestamp);
+    owner.transfer(address(this).balance);
+}
+
+}
